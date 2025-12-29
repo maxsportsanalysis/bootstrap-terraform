df5c885722c298b5b5b23c965ae5adbd136c36bf
Maxim Cilek <maxssportsanalysis@gmail.com>
2025-12-29 07:01:36 -0600

    GitHub Actions Initialization

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
-resource "aws_iam_openid_connect_provider" "github" {
-  url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = [
-    "sts.amazonaws.com"
-  ]
-
-  thumbprint_list = [
-    "6938fd4d98bab03faadb97b34396831e3780aea1"
-  ]
-}
+# resource "aws_iam_openid_connect_provider" "github" {
+#   url = "https://token.actions.githubusercontent.com"
+# 
+#   client_id_list = [
+#     "sts.amazonaws.com"
+#   ]
+# 
+#   thumbprint_list = [
+#     "6938fd4d98bab03faadb97b34396831e3780aea1"
+#   ]
+# }
 
 # "Federated": "arn:aws:iam::012345678910:oidc-provider/token.actions.githubusercontent.com"
 resource "aws_iam_role" "github_oidc" {
