2e8e881095131bace8bebcfd4e18e345e1fa1a5a
Maxim Cilek <maxssportsanalysis@gmail.com>
2025-12-29 23:38:35 -0600

    GitHub Actions Initialization

--- main.tf
+++ main.tf
@@ -1,3 +1,39 @@
+data "aws_iam_policy_document" "github_oidc_trust" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRoleWithWebIdentity"
+    ]
+
+    principals {
+      type        = "Federated"
+      identifiers = [
+        "arn:aws:iam::242201314218:oidc-provider/token.actions.githubusercontent.com"
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = [
+        "repo:maxsportsanalysis/bootstrap-terraform:*"
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_ci" {
+  name               = "GitHubTerraformRole"
+  assume_role_policy = data.aws_iam_policy_document.github_oidc_trust.json
+}
+
 data "aws_iam_policy_document" "bootstrap_assume_role" {
   statement {
     effect = "Allow"
