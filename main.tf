1ba8c86d5f57f10dd55bf365d1a6f30a1eaee149
Maxim Cilek <maxssportsanalysis@gmail.com>
2025-12-31 00:35:06 -0600

    GitHub Actions Initialization

--- main.tf
+++ main.tf
@@ -1,40 +1,4 @@
-data "aws_iam_policy_document" "github_oidc_trust" {
-  statement {
-    effect = "Allow"
-
-    actions = [
-      "sts:AssumeRoleWithWebIdentity"
-    ]
-
-    principals {
-      type        = "Federated"
-      identifiers = [
-        "arn:aws:iam::242201314218:oidc-provider/token.actions.githubusercontent.com"
-      ]
-    }
-
-    condition {
-      test     = "StringEquals"
-      variable = "token.actions.githubusercontent.com:aud"
-      values   = ["sts.amazonaws.com"]
-    }
-
-    condition {
-      test     = "StringLike"
-      variable = "token.actions.githubusercontent.com:sub"
-      values   = [
-        "repo:maxsportsanalysis/bootstrap-terraform:ref:refs/heads/main"
-      ]
-    }
-  }
-}
-
-resource "aws_iam_role" "github_ci" {
-  name               = "GitHubTerraformRole"
-  assume_role_policy = data.aws_iam_policy_document.github_oidc_trust.json
-}
-
-data "aws_iam_policy_document" "bootstrap_assume_role" {
+data "aws_iam_policy_document" "breakglass_trust_policy" {
   statement {
     effect = "Allow"
 
@@ -48,5 +12,16 @@ data "aws_iam_policy_document" "bootstrap_assume_role" {
         "arn:aws:iam::242201314218:user/bootstrap"
       ]
     }
+
+    condition {
+      test     = "Bool"
+      variable = "aws:MultiFactorAuthPresent"
+      values   = ["true"]
+    }
   }
+}
+
+resource "aws_iam_role" "breakglass" {
+  name               = "BreakGlassRoleTrustPolicy"
+  assume_role_policy = data.aws_iam_policy_document.breakglass_trust_policy.json
 }
