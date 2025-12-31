5e73b1f0d951e67f2bf968bf28231436132a529b
Maxim Cilek <maxssportsanalysis@gmail.com>
2025-12-31 09:44:45 -0600

    GitHub Actions Initialization

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "terraform_state" {
   bucket = "maxsportsanalysis-terraform-state-bucket"
 }
 
-resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
+resource "aws_s3_bucket_versioning" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
   versioning_configuration {
     status = "Enabled"
