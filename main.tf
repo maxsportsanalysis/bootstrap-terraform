206ab7f1635e841a550a913b95891978b2715c20
Maxim Cilek <maxssportsanalysis@gmail.com>
2025-12-31 08:56:02 -0600

    GitHub Actions Initialization

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
 
 resource "aws_s3_bucket_object_lock_configuration" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
-  object_lock_enabled = "true"
+  object_lock_enabled = "Enabled"
 
   rule {
     default_retention {
