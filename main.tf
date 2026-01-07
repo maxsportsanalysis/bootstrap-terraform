1c56c73dd6a17f82f08ec8619e5b0db75e1fa288
Maxim Cilek <maxssportsanalysis@gmail.com>
2026-01-07 09:00:00 -0600

    Setup config files

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "terraform_bucket" {
-  source              = "../../modules/s3_bucket"
+  source              = "modules/s3_bucket"
   bucket_name         = var.terraform_bucket_name
   versioning_enabled  = var.terraform_bucket_versioning_enabled
   object_lock_enabled = var.terraform_bucket_object_lock_enabled
